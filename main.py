62cd63b965a893d9dde36b7b29bf37263b9ff58c
janelim0414 <janelim801@gmail.com>
2024-05-21 15:13:50 -0400

    make directory paths variable

--- main.py
+++ main.py
@@ -10,23 +10,20 @@ from pylatex import Document, Section, Subsection, Tabular, Math, TikZ, Axis, \
     Plot, Figure, Matrix, Alignat
 from pylatex.utils import italic, bold
 import os
+import sys
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    N = 10  # set number of documents to convert
+def converter(src, dst, n):
     EMAIL = "Email*"
     PHONE = "Phone Number*"
     COMPANY = "Please enter your organization name*"
     BILLING = "Billing Information"
     SKIP = False
     ASSIGN = False
-    for i in range(1, N):
-        filename = '/Users/jane/new companies/' + str(i) + '.docx'
+    for i in range(1, n + 1):
+        filename = src + str(i) + '.docx'
         text = textract.process(filename)
         text = text.decode("utf-8")
         text = text.split('\n')
-        # with open(text, 'rb') as f:
         doc = Document(documentclass='article', indent=False)
         for x in text:
             if x == EMAIL or x == PHONE:
@@ -41,7 +38,7 @@ if __name__ == '__main__':
                 SKIP = False
                 continue
             if x == BILLING:
-                doc.generate_pdf('/Users/jane/new companies/' + company, clean_tex=True)
+                doc.generate_pdf(dst + company, clean_tex=True)
             if ASSIGN and x:
                 ASSIGN = False
                 doc.append(str(x) + '\n')
@@ -51,11 +48,12 @@ if __name__ == '__main__':
             else:
                 doc.append(str(x) + '\n')
 
-    #
-    # section = Section('hello')
-    # subsection = Subsection('my name is Jane')
-    # section.append(subsection)
-    # doc.append(section)
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    src = sys.argv[1]  # directory where word documents are stored
+    dst = sys.argv[2]  # directory where pdf documents will be stored
+    n = int(sys.argv[3])  # number of files to be converted
+    converter(src, dst, n)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
